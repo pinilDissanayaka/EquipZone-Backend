0b81c15405d190b895b60c5ef024315111d223a8
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-01-09 16:48:19 +0530

    add the db connetor

---
+++ database.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
+from dotenv import load_dotenv, find_dotenv
+import os
+
+
+load_dotenv(find_dotenv())
+
+
+engine=create_engine(url=os.getenv("DATABASE_URL"))
+
+Base=declarative_base()
+
+local_session=sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+session=local_session()
---
+++ models.py
@@ -0,0 +1,16 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Text, BigInteger
+from uuid import uuid4
+
+
+class User(Base):
+    __tablename__="user"
+
+    user_id = Column(BigInteger, primary_key=True, index=True, default=uuid4)
+    username = Column(String(length=50), unique=True)
+    password = Column(String(length=1000))
+    nic = Column(String(length=50))
+    email = Column(String(length=50))
+    phone = Column(String(length=50))
+
+
