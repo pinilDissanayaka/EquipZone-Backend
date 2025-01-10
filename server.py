a49f02777e0e6b0ba4ba6525e99a9c70fc16a7ad
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-01-10 08:27:38 +0530

    add user get all equipments

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from routes.auth import router as auth_router
+from routes.auth import router as auth_routers
+from routes.user import router as user_routers
 
 app = FastAPI()
 
-app.include_router(auth_router)
+app.include_router(auth_routers)
+app.include_router(user_routers)
 
 
 @app.get("/")
