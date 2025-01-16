1773bff029df5dd9338b0c3bd88afbb6228833c2
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-01-16 09:27:09 +0530

    add user ends

--- routes/admin.py
+++ routes/admin.py
@@ -115,6 +115,62 @@ async def add_users(addUser: AddUser):
             content="User added successfully.",
             status_code=status.HTTP_201_CREATED
         )
+    
+@router.get("/users")
+async def get_users():
+    all_users = session.query(User).all()
+
+    serialized_users_data = [user.to_dict() for user in all_users]
+
+    return JSONResponse(
+        content=serialized_users_data,
+        status_code=status.HTTP_200_OK
+    )
+
+
+@router.put("/users/{user_id}")
+async def update_user(user_id:int, addUser:AddUser):
+    existing_user = session.query(User).filter_by(id=user_id)
+
+    if existing_user:
+        existing_user.username=addUser.username
+        existing_user.password=addUser.password
+        existing_user.nic=addUser.nic
+        existing_user.email=addUser.email
+        existing_user.phone=addUser.phone
+
+        session.commit()
+
+        return Response(
+            content="User Update Successfully.",
+            status_code=200
+        )
+    else:
+        return HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Object does not exist"
+        )
+    
+@router.delete("/users/{user_id}")
+async def update_user(user_id:int):
+    existing_user = session.query(User).filter_by(id=user_id)
+
+    if existing_user:
+        session.delete(existing_user)
+
+        session.commit()
+
+        return Response(
+            content="User Delete Successfully.",
+            status_code=200
+        )
+    else:
+        return HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Object does not exist"
+        )
+
+
 
 
 
