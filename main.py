f74b4d66a38660f8723df635219fe153322f3cc0
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-02-02 14:36:40 +0530

    add cors

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from server import app
 import uvicorn
 from database import engine, Base, session
-from models import User
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
